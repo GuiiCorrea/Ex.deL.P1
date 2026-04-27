42a4af540a8da30b5e56139299453136e3589a6b
GuiiCorrea <guilhermescorrea2008@gmail.com>
2026-04-27 20:07:19 -0300

    Change form method from POST to GET

--- calcular.php
+++ calcular.php
@@ -7,12 +7,12 @@
 </head>
 <body>
     <?php
-    if(isset($_POST['nota1']) && isset($_POST['nota2']) && isset($_POST['nota3']) && isset($_POST['nota4'])) {
-        $nome = $_POST['nameAluno'];
-        $nota1 = $_POST['nota1'];
-        $nota2 = $_POST['nota2'];
-        $nota3 = $_POST['nota3'];
-        $nota4 = $_POST['nota4'];
+    if(isset($_GET['nota1']) && isset($_GET['nota2']) && isset($_GET['nota3']) && isset($_GET['nota4'])) {
+        $nome = $_GET['nameAluno'];
+        $nota1 = $_GET['nota1'];
+        $nota2 = $_GET['nota2'];
+        $nota3 = $_GET['nota3'];
+        $nota4 = $_GET['nota4'];
 
         $media = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
         echo "A média de " . $nome . " é: " . $media;
